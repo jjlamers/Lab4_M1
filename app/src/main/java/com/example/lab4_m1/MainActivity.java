d7254c338e0abd6f8d999d47e0baddee438c1d8d
jake lamers <jakelamers931@gmail.com>
2021-10-10 21:57:03 -0500

    Completed through M1-7

--- app/src/main/java/com/example/lab4_m1/MainActivity.java
+++ app/src/main/java/com/example/lab4_m1/MainActivity.java
@@ -5,18 +5,29 @@ import 	android.util.Log;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.start);
     }
 
     public void mockFileDownloader() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Downloading...");
+            }
+        });
+
         for (int downloadProgress = 0; downloadProgress <= 100;downloadProgress=downloadProgress+10) {
             Log.d(TAG,"Download Progress: " + downloadProgress + "%");
             try {
@@ -26,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Start");
+            }
+        });
     }
 
     public void startDownload(View view){
